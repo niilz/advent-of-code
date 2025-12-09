800434a73082758dd20f4092888ae5dc85e79b05
niilz <niilz@outlook.de>
2025-12-09 22:10:42 +0100

    Attempt with VecDeque: panics... Bug in VD???

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,37 +1,37 @@
 use std::collections::VecDeque;
 
 fn main() {
-    let input = inputs::fetch_input(1);
+    let inputs = inputs::fetch_input(1);
 
     let mut wheel: VecDeque<usize> = (0..100).collect();
     wheel.rotate_left(50);
 
-    let (_, zeros) =
-        input[0..10]
-            .iter()
-            .map(to_number)
-            .fold((50usize, 0), |(current, mut zeros), clicks| {
-                println!("now: {current} turning {clicks}, zeros: {zeros}");
-                dbg!(current, zeros, clicks);
-                let cur = turn(current, clicks);
-                if cur == 0 {
-                    zeros += 1;
-                }
-                println!("after: {cur}, zeros: {zeros}");
-                (cur, zeros)
-            });
+    let mut zeros = 0;
+    for input in &inputs[..105] {
+        let (clicks, rotation) = action(input);
+        rotation(&mut wheel, clicks);
+        let first = wheel.get(0).expect("read at idx 0");
+        dbg!(first);
+        if first == &0 {
+            zeros += 1;
+        }
+        dbg!(input);
+    }
     dbg!(zeros);
 }
 
-fn to_number(turn: impl AsRef<str>) -> isize {
+fn action(turn: impl AsRef<str>) -> (usize, fn(&mut VecDeque<usize>, usize)) {
     let sign = &turn.as_ref()[0..1];
-    dbg!(sign, turn.as_ref());
     let clicks = &turn.as_ref()[1..];
-    let sign = if sign == "R" { 1 } else { -1 };
-    dbg!(sign, clicks);
-    clicks.parse::<isize>().expect("parse click") * sign
-}
-
-fn turn(current: usize, clicks: isize) -> usize {
-    (current + clicks) % 99
+    let clicks = clicks.parse::<usize>().expect("parse click");
+    if sign == "R" {
+        (clicks, |mut vd, clicks| {
+            dbg!(vd.len(), clicks);
+            VecDeque::rotate_left(&mut vd, clicks)
+        })
+    } else {
+        (clicks, |mut vd, clicks| {
+            VecDeque::rotate_right(&mut vd, clicks)
+        })
+    }
 }
