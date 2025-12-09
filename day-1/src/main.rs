67f52bc34d98079dd83f9623a5b87d7118eed5b0
niilz <niilz@outlook.de>
2025-12-09 22:33:35 +0100

    check bounds manually: not working yet

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,37 +1,34 @@
-use std::collections::VecDeque;
-
 fn main() {
     let inputs = inputs::fetch_input(1);
 
-    let mut wheel: VecDeque<usize> = (0..100).collect();
-    wheel.rotate_left(50);
+    let mut current = 50;
 
     let mut zeros = 0;
-    for input in &inputs[..105] {
-        let (clicks, rotation) = action(input);
-        rotation(&mut wheel, clicks);
-        let first = wheel.get(0).expect("read at idx 0");
-        dbg!(first);
-        if first == &0 {
+    for clicks in &inputs[..10] {
+        dbg!(clicks);
+        current = turn(current, clicks);
+        if current == 0 {
             zeros += 1;
         }
-        dbg!(input);
     }
     dbg!(zeros);
 }
 
-fn action(turn: impl AsRef<str>) -> (usize, fn(&mut VecDeque<usize>, usize)) {
+fn turn(current: usize, turn: impl AsRef<str>) -> usize {
     let sign = &turn.as_ref()[0..1];
     let clicks = &turn.as_ref()[1..];
-    let clicks = clicks.parse::<usize>().expect("parse click");
-    if sign == "R" {
-        (clicks, |mut vd, clicks| {
-            dbg!(vd.len(), clicks);
-            VecDeque::rotate_left(&mut vd, clicks)
-        })
+    let clicks = clicks.parse::<isize>().expect("parse click");
+    let sign = if sign == "R" { 1 } else { -1isize };
+    let num = current as isize + clicks * sign;
+    dbg!(current, sign, clicks, num);
+    let res = if num < 0 {
+        // num is negative
+        (100 + num) as usize
+    } else if num > 99 {
+        (num - 100) as usize
     } else {
-        (clicks, |mut vd, clicks| {
-            VecDeque::rotate_right(&mut vd, clicks)
-        })
-    }
+        num as usize
+    };
+    dbg!(res);
+    res
 }
